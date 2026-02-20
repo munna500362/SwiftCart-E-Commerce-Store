65ca7b9a1e192c77afd30840041bf8400865f5e0
mehedi hasan munna <mehedihasan500362@gmail.com>
2026-02-20 22:49:22 +0600

    card section start

--- index.js
+++ index.js
@@ -1,11 +1,104 @@
-function productBtn (){
+function productBtn  (){
     fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
-    .then((data) => AllProductBtn(data))
+    .then((data) => productBtnDefined(data))
 }
 
-function AllProductBtn(btns){
-   console.log(btns)
+function productBtnDefined(data){
+  
+    data.forEach((item) => {
+        // console.log(item)
+    
+        
+        const productBtnID = document.getElementById('product-btn');
+        // console.log(item)
+        const createButton = document.createElement('div');
+        // // console.log(lesson)
+        createButton.innerHTML= 
+        `
+            <button onclick = "productCategory()" class=" btn hover:border-[#422AD5] hover:bg-[#422AD5] hover:text-white">${item} </button>
+        `
+
+        productBtnID.appendChild(createButton);
+    } )
+
+}
+
+function productCategory (){
+    const url = `https://fakestoreapi.com/products`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => productCategoryCard(data))
+}
+
+function productCategoryCard (categorys){
+    console.log(categorys)
+    categorys.forEach ((category) =>{
+        const categoryCard = document.getElementById('category-card');
+
+        categoryCardDetails = document.createElement('div');
+        categoryCardDetails.classList.add('gap-3');
+        categoryCardDetails.innerHTML = 
+        `
+            <div class="card bg-base-100 shadow-sm">
+                        <figure class="px-5 md:px-10 py-5 md:py-10 bg-gray-500">
+                            <img class="rounded-xl w-1/2 h-40 object-contain"
+                            src="${category.image}"
+                            alt="Shoes"
+                             />
+                        </figure>
+                        <div class="text-start p-3 md:p-5 space-y-3 md:space-y-5">
+                            <div class="flex justify-between"> 
+                                <button class="rounded-full px-2 py-1 text-[#4F46E5] bg-gray-300 "> ${category.category}</button>
+                                <p><i class="fa-solid fa-star text-orange-300 bg-orange-300"></i> <span> ${category.rating.rate} (${category.rating.count})</span></p>
+                            </div>
+                        
+                            <div class="space-y-2">
+                                <h3 class="text-xl font-semibold truncate w-full">${category.title}</h3>
+                                <p class = "text-xl font-bold">$${category.price}</p>
+                            </div>
+                            <div class="flex justify-between gap-5">
+                                    <button class="btn">
+                                    <i class="fa-regular fa-eye"></i>
+                                    Buy Now</button>
+                                    <button class="btn btn-primary">Add to Card</button>
+                            </div>
+                            
+                            </div>
+                        </div>
+            </div>
+        `
+        categoryCard.appendChild(categoryCardDetails)
+    })
 }
 
 productBtn()
+
+
+// category
+// : 
+// "men's clothing"
+// description
+// : 
+// "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+// id
+// : 
+// 1
+// image
+// : 
+// "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
+// price
+// : 
+// 109.95
+// rating
+// : 
+// {rate: 3.9, count: 120}
+// title
+// : 
+// "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+
+
+// "rating": {
+// "rate": 3.9,
+// "count": 120
+// }
