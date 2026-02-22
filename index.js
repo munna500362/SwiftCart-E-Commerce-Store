fd047a212687a7ad97f51c1fdd50529c174772e9
mehedi hasan munna <mehedihasan500362@gmail.com>
2026-02-22 09:41:50 +0600

    card section

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+// Product button start
+
 function productBtn  (){
     fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
     .then((data) => productBtnDefined(data))
 }
 
+
 function productBtnDefined(data){
-  
+
     data.forEach((item) => {
-        // console.log(item)
-    
-        
+
         const productBtnID = document.getElementById('product-btn');
-        // console.log(item)
-        const createButton = document.createElement('div');
-        // // console.log(lesson)
-        createButton.innerHTML= 
-        `
-            <button onclick = "productCategory()" class=" btn hover:border-[#422AD5] hover:bg-[#422AD5] hover:text-white">${item} </button>
-        `
+            productBtnID.innerHTML = ''
+        const button = document.createElement('button');
 
-        productBtnID.appendChild(createButton);
-    } )
+        button.innerText = item;
+        button.className = "btn hover:border-[#422AD5] hover:bg-[#422AD5] hover:text-white";
 
+        button.addEventListener("click", () => {
+            CategoryCard(item);
+        });
+
+        productBtnID.appendChild(button);
+    });
 }
 
-function productCategory (){
+// Product button end
+
+// All Category card start
+function productAllCategory (){
     const url = `https://fakestoreapi.com/products`;
     fetch(url)
     .then((res) => res.json())
-    .then((data) => productCategoryCard(data))
+    .then((data) => productAllCategoryCard(data))
 }
 
-function productCategoryCard (categorys){
+
+
+function productAllCategoryCard (categorys){
     console.log(categorys)
     categorys.forEach ((category) =>{
         const categoryCard = document.getElementById('category-card');
@@ -71,6 +78,67 @@ function productCategoryCard (categorys){
         categoryCard.appendChild(categoryCardDetails)
     })
 }
+
+// All Category card end
+
+
+
+// Category card Start
+
+function CategoryCard (id){
+
+    // console.log(id)
+    const url = `https://fakestoreapi.com/products/category/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => CategoryCardDetails(data))
+}
+
+
+
+function CategoryCardDetails (categorys){
+    // console.log(categorys)
+    categorys.forEach ((category) =>{
+        const categoryCards = document.getElementById('category-card');
+        
+        CardDetails = document.createElement('div');
+        CardDetails.classList.add('gap-3');
+        CardDetails.innerHTML = 
+        `
+            <div class="card bg-base-100 shadow-sm">
+                        <figure class="px-5 md:px-10 py-5 md:py-10 bg-gray-500">
+                            <img class="rounded-xl w-1/2 h-40 object-contain"
+                            src="${category.image}"
+                            alt="Shoes"
+                             />
+                        </figure>
+                        <div class="text-start p-3 md:p-5 space-y-3 md:space-y-5">
+                            <div class="flex justify-between"> 
+                                <button class="rounded-full px-2 py-1 text-[#4F46E5] bg-gray-300 "> ${category.category}</button>
+                                <p><i class="fa-solid fa-star text-orange-300 bg-orange-300"></i> <span> ${category.rating.rate} (${category.rating.count})</span></p>
+                            </div>
+                        
+                            <div class="space-y-2">
+                                <h3 class="text-xl font-semibold truncate w-full">${category.title}</h3>
+                                <p class = "text-xl font-bold">$${category.price}</p>
+                            </div>
+                            <div class="flex justify-between gap-5">
+                                    <button class="btn">
+                                    <i class="fa-regular fa-eye"></i>
+                                    Buy Now</button>
+                                    <button class="btn btn-primary">Add to Card</button>
+                            </div>
+                            
+                            </div>
+                        </div>
+            </div>
+        `
+        categoryCards.appendChild(CardDetails)
+    })
+}
+
+
+
 
 productBtn()
 
